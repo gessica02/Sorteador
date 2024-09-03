b9d8709810603d1ffee56352d9d07a5fdb566181
Gessica J Machado <gessica.machado26@gmail.com>
2024-09-03 08:57:58 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+
+
+    for (let i = 0; i < quantidade; i++){
+        numero = gerarNumeroAleatorio(de, ate);
+        while(sorteados.includes(numero)){
+            numero = gerarNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+}
+
+function reiniciar(){
+
+    class="container__botao-desabilitado"
+
+}
+
+function gerarNumeroAleatorio (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
