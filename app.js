a396766c1ab5ead8e986104a622f2939d0037ad1
Gessica J Machado <gessica.machado26@gmail.com>
2024-09-03 10:10:24 -0300

    Botão reiniciar e melhorias.

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
+    if (de>=ate){
+        alert('O campo "Do número" deve ser menor que o campo "Até o número". Por favor, verifique!');
+        return;
+    }
+    if(quantidade>(ate-de+1)){
+        alert('O número informado no campo "Quantidade" é maior que o intervalo fornecido nos campos "Do número" e "Até o número". Por favor verifique!');
+        return;
+    }
+
     let sorteados = [];
     let numero;
 
@@ -17,14 +26,30 @@ function sortear(){
 
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+
+    alterarBotao();
 }
 
 function reiniciar(){
-
-    class="container__botao-desabilitado"
-
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML= '<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>';
+    alterarBotao();
 }
 
 function gerarNumeroAleatorio (min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function alterarBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+
 }
